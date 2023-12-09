7da0ce577d97895544d80633d1cac477580f6b5f
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-12-09 20:11:51 +0300

    I see

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,16 @@
 # Install Nginx if not already installed
 if ! command -v nginx &> /dev/null
 then
-  echo "Installing Nginx..."
-  sudo apt-get update
-  sudo apt-get install -y nginx
+ echo "Installing Nginx..."
+ sudo apt-get update
+ sudo apt-get install -y nginx
 fi
 
 # Create necessary directories
 echo "Creating directories..."
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Create a fake HTML file for testing
 echo "Creating test HTML file..."
@@ -20,14 +21,14 @@ echo "<html>
  <head>
  </head>
  <body>
-   Holberton School
+  Holberton School
  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create symbolic link
 echo "Creating symbolic link..."
 if [ -h /data/web_static/current ]; then
-  sudo rm /data/web_static/current
+ sudo rm /data/web_static/current
 fi
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
@@ -39,26 +40,26 @@ sudo chown -R ubuntu:ubuntu /data/
 echo "Updating Nginx configuration..."
 sudo bash -c 'cat << EOF > /etc/nginx/sites-available/default
 server {
-   listen 80 default_server;
-   listen [::]:80 default_server;
-   add_header X-Served-By $HOSTNAME;
-   root  /var/www/html;
-   index index.html index.htm;
+  listen 80 default_server;
+  listen [::]:80 default_server;
+  add_header X-Served-By $HOSTNAME;
+  root /var/www/html;
+  index index.html index.htm;
 
-   location /hbnb_static {
-       alias /data/web_static/current;
-       index index.html index.htm;
-   }
+  location /hbnb_static {
+      alias /data/web_static/current;
+      index index.html index.htm;
+  }
 
-   location /redirect_me {
-       return 301 http://cuberule.com/;
-   }
+  location /redirect_me {
+      return 301 http://cuberule.com/;
+  }
 
-   error_page 404 /404.html;
-   location /404 {
-     root /var/www/html;
-     internal;
-   }
+  error_page 404 /404.html;
+  location /404 {
+    root /var/www/html;
+    internal;
+  }
 }
 EOF'
 
