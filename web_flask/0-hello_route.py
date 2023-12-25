5ac2276f5b2df32dba098d98874e35a33c4c1321
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-12-26 00:24:21 +0300

    we see

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,14 @@
 starting a flask application
 """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_route():
+    """ Prints a Message when / is called """
     return "Hello HBNB!"
 
 if __name__ =='__main__':
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
